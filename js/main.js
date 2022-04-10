01d6beee3433875ee8934ba80dd0f93e0074b4a4
HenkSeed <adaner@inbox.ru>
2022-04-10 13:39:46 +0700

    Упростили код, создав унивирсальную процедуру переключения элементов

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
+// Выбираем в псевдо-массив все закладки
 const markStones = document.querySelectorAll('.mark-stone');
+// Выбираем в псевдо-массив все заголовки выбора
 const choiceTexts = document.querySelectorAll('.choice-text');
+// Выбираем в псевдо-массив все строки картинок
 const choiceImgs = document.querySelectorAll('.choice-imgs');
-console.log('choiceImgs: ', choiceImgs);
 
+// Перебираем все закладки
 markStones.forEach((markStone) => {
+	// На каждую закладку навешиваем обработчик нажатий мыши
 	markStone.addEventListener('click', (event) => {
+		// Получаем значение дата-атрибута "direction" у нажатой закладки
 		const dataDirection = markStone.dataset.direction;
-		console.log('dataDirection: ', dataDirection);
 
+		// Описание процедуры обработки переключаемых элементов
+		// аргументы - массив элементов, значение дата-атрибута
+		const dataChoice = (array, dataValue) => {
+			array.forEach((dataItem) => {
+				// Если элемент массива имеет дата-атрибут как в нажатой закладке
+				if (dataItem.dataset.direction === dataValue) {
+					// Делаем его видимым
+					dataItem.classList.remove('hidden');
+					// Все остальные элементы массива скрываем
+				} else {
+					// скрываем
+					dataItem.classList.add('hidden');
+				}
+			});
+		};
+
+		// Обрабатываем нажатие таба (меняем активную вкладку)
 		markStones.forEach((item) => {
 			if (item === event.target) {
 				item.classList.add('active');
@@ -15,21 +36,9 @@ markStones.forEach((markStone) => {
 				item.classList.remove('active');
 			}
 		});
-
-		choiceTexts.forEach((choiceText) => {
-			if (choiceText.dataset.direction === dataDirection) {
-				choiceText.classList.remove('hidden');
-			} else {
-				choiceText.classList.add('hidden');
-			}
-		});
-
-		choiceImgs.forEach((choiceImg) => {
-			if (choiceImg.dataset.direction === dataDirection) {
-				choiceImg.classList.remove('hidden');
-			} else {
-				choiceImg.classList.add('hidden');
-			}
-		});
+		// Вызываем процедуру обработки переключения текста заголовка выбора
+		dataChoice(choiceTexts, dataDirection);
+		// Вызываем процедуру обработки переключения строки изображений
+		dataChoice(choiceImgs, dataDirection);
 	});
 });
